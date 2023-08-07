64808922521a6a14e3854958b009e08ae51ab6fe
Marcos Alexandre <marcosxx1.ma@gmail.com>
2023-08-07 12:16:53 -0400

    refactor: Working in memory API

--- src/events.crontroller.ts
+++ src/events.crontroller.ts
@@ -10,39 +10,53 @@ import {
 } from '@nestjs/common';
 import { CreateEventDto } from './create-event.dto';
 import { UpdateEventDto } from './update-event.dto';
+import { Event } from './event.entity';
 
 @Controller('events')
 export class EventsController {
+  private events: Event[] = [];
+
   @Get()
   findAll() {
-    return [
-      {
-        id: 1,
-        name: 'Event 1',
-      },
-      {
-        id: 2,
-        name: 'Event 2',
-      },
-    ];
+    return this.events;
   }
 
   @Get(':id')
   findOne(@Param('id') id) {
-    return `This action returns a #${id} event`;
+    const event = this.events.find((event) => event.id === +id);
+    return event;
   }
 
   @Post()
   create(@Body() body: CreateEventDto) {
-    return body;
+    const newEvent = {
+      ...body,
+      when: new Date(body.when),
+      id: this.events.length + 1,
+      name: body.name.toUpperCase(),
+      description: body.description.toUpperCase(),
+    };
+    this.events.push(newEvent);
   }
 
   @Patch(':id')
   update(@Param('id') id, @Body() body: UpdateEventDto) {
-    return body;
+    const index = this.events.indexOf(
+      this.events.find((event) => event.id === +id),
+    );
+
+    this.events[index] = {
+      ...this.events[index],
+      ...body,
+      when: body.when ? new Date(body.when) : this.events[index].when,
+    };
+    return this.events[index];
   }
 
   @Delete(':id')
   @HttpCode(204)
-  delete(@Param('id') id) { }
+  delete(@Param('id') id) {
+    this.events.findIndex((event) => event.id === +id);
+    this.events.splice(id - 1, 1);
+  }
 }
