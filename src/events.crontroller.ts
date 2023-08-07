0adbc7127a126c920301e12554a4db0da4de19e0
Marcos Alexandre <marcosxx1.ma@gmail.com>
2023-08-07 10:53:59 -0400

    feat: add updateEventDto on patch

--- src/events.crontroller.ts
+++ src/events.crontroller.ts
@@ -1,9 +1,31 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateEventDto } from './create-event.dto';
+import { UpdateEventDto } from './update-event.dto';
 
 @Controller('events')
 export class EventsController {
   @Get()
-  findAll() { }
+  findAll() {
+    return [
+      {
+        id: 1,
+        name: 'Event 1',
+      },
+      {
+        id: 2,
+        name: 'Event 2',
+      },
+    ];
+  }
 
   @Get(':id')
   findOne(@Param('id') id) {
@@ -11,11 +33,16 @@ export class EventsController {
   }
 
   @Post()
-  create() { }
+  create(@Body() body: CreateEventDto) {
+    return body;
+  }
 
-  @Patch()
-  update() { }
+  @Patch(':id')
+  update(@Param('id') id, @Body() body: UpdateEventDto) {
+    return body;
+  }
 
   @Delete(':id')
+  @HttpCode(204)
   delete(@Param('id') id) { }
 }
