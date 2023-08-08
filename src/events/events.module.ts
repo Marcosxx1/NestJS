748ff31c2c98a6ccae1733133f45e69297ce2f62
Marcos Alexandre <marcosxx1.ma@gmail.com>
2023-08-08 16:41:22 -0400

    feat: Add EvetsService as a provider on events.module @Module

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { EventsController } from './events.crontroller';
 import { Event } from './event.entity';
 import { Attendee } from './attendee.entity';
+import { EventsService } from './events.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Event, Attendee])],
   controllers: [EventsController],
+  providers: [EventsService],
 })
 // eslint-disable-next-line prettier/prettier
 export class EventsModule { }
