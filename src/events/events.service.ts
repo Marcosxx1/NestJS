f468fb55d4df16348e5c94879f0c86d2eeb68bde
Marcos Alexandre <marcosxx1.ma@gmail.com>
2023-08-08 16:40:47 -0400

    feat: Create Events service with getEventsBaseQuery and getEvent methods

---
+++ src/events/events.service.ts
@@ -0,0 +1,31 @@
+/* eslint-disable prettier/prettier */
+import { Repository } from 'typeorm';
+import { Event } from './event.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Injectable, Logger } from '@nestjs/common';
+
+@Injectable()
+export class EventsService {
+  private readonly logger = new Logger(EventsService.name);
+
+  constructor(
+    @InjectRepository(Event)
+    private readonly eventsRepository: Repository<Event>,
+  ) { }
+
+  private getEventsBaseQuery() {
+    return this.eventsRepository
+      .createQueryBuilder('e')
+      .orderBy('e.id', 'DESC');
+  }
+
+  public async getEvent(id: number): Promise<Event | undefined> {
+    const query = await this.getEventsBaseQuery()
+      .andWhere('e.id = :id', { id });
+
+    this.logger.debug(query.getSql());
+
+    return await query.getOne();
+
+  }
+}
