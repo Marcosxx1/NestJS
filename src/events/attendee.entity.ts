19b418e63a291359b802aa82a93e5abf907baf11
Marcos Alexandre <marcosxx1.ma@gmail.com>
2023-08-08 12:44:39 -0400

    feat: Entity for attendee

---
+++ src/events/attendee.entity.ts
@@ -0,0 +1,23 @@
+import {
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { Event } from './event.entity';
+
+@Entity()
+export class Attendee {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @ManyToOne(() => Event, (event) => event.attendees, {
+    nullable: false,
+  })
+  @JoinColumn()
+  event: Event;
+}
