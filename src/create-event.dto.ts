64b258aa5891d3585396a1cc9bf4f1a2c9e59cae
Marcos Alexandre <marcosxx1.ma@gmail.com>
2023-08-08 08:05:31 -0400

    feat: Add decorators for validation

--- src/create-event.dto.ts
+++ src/create-event.dto.ts
@@ -1,6 +1,17 @@
+import { IsDateString, IsNotEmpty, IsString, Length } from 'class-validator';
+
 export class CreateEventDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(5, 255, { message: 'The name length is wrong' })
   name: string;
+
+  @Length(5, 255, { message: 'The description length is wrong' })
   description: string;
+
+  @IsDateString()
   when: string;
+
+  @Length(5, 255, { message: 'The name length is wrong' })
   address: string;
 }
