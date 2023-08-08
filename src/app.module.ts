710047ed976f225a9595ef34bcdf816564f87787
Marcos Alexandre <marcosxx1.ma@gmail.com>
2023-08-08 10:49:04 -0400

    refactor: use environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,18 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EventsModule } from './events/events.module';
 import { Event } from './events/event.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(), //Carrega as variaveis de ambiente
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'example',
-      database: 'nest-events',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [Event],
       synchronize: true,
     }),
