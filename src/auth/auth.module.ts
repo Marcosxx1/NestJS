0895ddc3607f516ad7bb63213b2e568d34b0daf8
Marcos Alexandre <marcosxx1.ma@gmail.com>
2023-08-10 12:02:57 -0400

    feat: Use registerAsync to get env and exp

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,10 +5,20 @@ import { LocalStrategy } from './local.strategy';
 import { AuthController } from './auth.controller';
 import { Profile } from './profile.entity';
 import { User } from './user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Profile, User])],
+  imports: [TypeOrmModule.forFeature([Profile, User]),
+  JwtModule.registerAsync({
+    useFactory: () => ({
+      secret: process.env.AUTH_SECRET,
+      signOptions: {
+        expiresIn: '60m'
+      }
+    })
+  })],
   controllers: [AuthController],
-  providers: [LocalStrategy],
+  providers: [LocalStrategy, AuthService],//tudo que é @Injectable é um provider
 })
 export class AuthModule { }
