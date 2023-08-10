7453d13af1d96d2af01381f7b85c28b82dbf8af5
Marcos Alexandre <marcosxx1.ma@gmail.com>
2023-08-10 10:16:04 -0400

    fix: Fix import of profile. Will correct path

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,12 +2,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LocalStrategy } from './local.strategy';
+import { AuthController } from './auth.controller';
 import { Profile } from './profile.entity';
 import { User } from './user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Profile, User])],
-  controllers: [],
+  controllers: [AuthController],
   providers: [LocalStrategy],
 })
 export class AuthModule { }
